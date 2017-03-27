7451b8bb0265049b854de131861557aa9d0bbe0d
Surya Setiyaputra <hello@setiyaputra.me>
2017-03-28 10:18:21 +1100

    update secrets.yml

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,11 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    # To set up the variable for our comment form
+    @post = Post.find(params[:id])
+    @comment = Comment.new
+    # Returns an array with all comments that relate to the post
+    @comments = @post.comments
   end
 
   # GET /posts/new
@@ -25,6 +30,7 @@ class PostsController < ApplicationController
   # POST /posts.json
   def create
     @post = Post.new(post_params)
+    @post.user = current_user
 
     respond_to do |format|
       if @post.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  resources :comments
-  resources :posts
+  root 'pages#home'
+
+  resources :posts, :shallow => true do
+    resources :comments
+  end
+  
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

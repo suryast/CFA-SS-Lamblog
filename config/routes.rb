422251777edf519af56166011ffc23e2c8d44a67
Surya Setiyaputra <hello@setiyaputra.me>
2017-03-28 16:54:33 +1100

    Implemented github-omniauth after previously failing to implement twitter-omniauth.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   resources :posts, :shallow => true do
     resources :comments
   end
-  
-  devise_for :users
+
+  devise_for :users, :controllers => { :omniauth_callbacks => "callbacks" }
+
+  #devise_scope :user do
+  #  delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  #end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
